afd8c69b61ba170b4168e923d5e5e9430287590c
François Voron <fvoron@gmail.com>
2022-11-22 17:09:36 +0100

    Add helper send methods

--- httpx_ws/_api.py
+++ httpx_ws/_api.py
@@ -1,5 +1,6 @@
 import base64
 import contextlib
+import json
 import secrets
 import typing
 
@@ -27,8 +28,24 @@ class WebSocketSession:
         self.stream = response.extensions["network_stream"]
         self.connection = wsproto.Connection(wsproto.ConnectionType.CLIENT)
 
-    async def send(self, event: wsproto.events.Event):
+    async def send(self, event: wsproto.events.Event) -> None:
         await self._send_event(event)
+
+    async def send_text(self, data: str) -> None:
+        event = wsproto.events.TextMessage(data=data)
+        await self.send(event)
+
+    async def send_bytes(self, data: bytes) -> None:
+        event = wsproto.events.BytesMessage(data=data)
+        await self.send(event)
+
+    async def send_json(self, data: typing.Any, mode: str = "text") -> None:
+        assert mode in ["text", "binary"]
+        serialized_data = json.dumps(data)
+        if mode == "text":
+            await self.send_text(serialized_data)
+        else:
+            await self.send_bytes(serialized_data.encode("utf-8"))
 
     async def receive(self) -> wsproto.events.Event:
         data = await self.stream.read(max_bytes=4096)
@@ -37,6 +54,7 @@ class WebSocketSession:
             if isinstance(event, wsproto.events.CloseConnection):
                 raise WebSocketDisconnect(event.code, event.reason)
             return event
+        raise HTTPXWSException()  # pragma: no cover
 
     async def close(self, code: int = 1000, reason: typing.Optional[str] = None):
         event = wsproto.events.CloseConnection(code, reason)
--- tests/test_api.py
+++ tests/test_api.py
@@ -24,10 +24,10 @@ async def test_upgrade_error():
                 pass
 
 
-@pytest.mark.asyncio
-async def test_send_message(
+@pytest.fixture
+def send_app(
     websocket_app_factory: Callable[[Callable], ASGIApp], on_receive_message: MagicMock
-):
+) -> ASGIApp:
     async def websocket_endpoint(websocket: WebSocket):
         await websocket.accept()
 
@@ -36,15 +36,104 @@ async def test_send_message(
 
         await websocket.close()
 
-    app = websocket_app_factory(websocket_endpoint)
+    return websocket_app_factory(websocket_endpoint)
 
-    async with httpx.AsyncClient(
-        base_url="http://localhost:8000", transport=ASGIWebSocketTransport(app)
-    ) as client:
-        async with aconnect_ws(client, "/ws") as ws:
-            await ws.send(wsproto.events.TextMessage(data="CLIENT_MESSAGE"))
 
-    on_receive_message.assert_called_once_with("CLIENT_MESSAGE")
+@pytest.mark.asyncio
+class TestSend:
+    async def test_send(
+        self,
+        websocket_app_factory: Callable[[Callable], ASGIApp],
+        on_receive_message: MagicMock,
+    ):
+        async def websocket_endpoint(websocket: WebSocket):
+            await websocket.accept()
+
+            message = await websocket.receive_text()
+            on_receive_message(message)
+
+            await websocket.close()
+
+        app = websocket_app_factory(websocket_endpoint)
+
+        async with httpx.AsyncClient(
+            base_url="http://localhost:8000", transport=ASGIWebSocketTransport(app)
+        ) as client:
+            async with aconnect_ws(client, "/ws") as ws:
+                await ws.send(wsproto.events.TextMessage(data="CLIENT_MESSAGE"))
+
+        on_receive_message.assert_called_once_with("CLIENT_MESSAGE")
+
+    async def test_send_text(
+        self,
+        websocket_app_factory: Callable[[Callable], ASGIApp],
+        on_receive_message: MagicMock,
+    ):
+        async def websocket_endpoint(websocket: WebSocket):
+            await websocket.accept()
+
+            message = await websocket.receive_text()
+            on_receive_message(message)
+
+            await websocket.close()
+
+        app = websocket_app_factory(websocket_endpoint)
+
+        async with httpx.AsyncClient(
+            base_url="http://localhost:8000", transport=ASGIWebSocketTransport(app)
+        ) as client:
+            async with aconnect_ws(client, "/ws") as ws:
+                await ws.send_text("CLIENT_MESSAGE")
+
+        on_receive_message.assert_called_once_with("CLIENT_MESSAGE")
+
+    async def test_send_bytes(
+        self,
+        websocket_app_factory: Callable[[Callable], ASGIApp],
+        on_receive_message: MagicMock,
+    ):
+        async def websocket_endpoint(websocket: WebSocket):
+            await websocket.accept()
+
+            message = await websocket.receive_bytes()
+            on_receive_message(message)
+
+            await websocket.close()
+
+        app = websocket_app_factory(websocket_endpoint)
+
+        async with httpx.AsyncClient(
+            base_url="http://localhost:8000", transport=ASGIWebSocketTransport(app)
+        ) as client:
+            async with aconnect_ws(client, "/ws") as ws:
+                await ws.send_bytes(b"CLIENT_MESSAGE")
+
+        on_receive_message.assert_called_once_with(b"CLIENT_MESSAGE")
+
+    @pytest.mark.parametrize("mode", ["text", "binary"])
+    async def test_send_json(
+        self,
+        mode: str,
+        websocket_app_factory: Callable[[Callable], ASGIApp],
+        on_receive_message: MagicMock,
+    ):
+        async def websocket_endpoint(websocket: WebSocket):
+            await websocket.accept()
+
+            message = await websocket.receive_json(mode=mode)
+            on_receive_message(message)
+
+            await websocket.close()
+
+        app = websocket_app_factory(websocket_endpoint)
+
+        async with httpx.AsyncClient(
+            base_url="http://localhost:8000", transport=ASGIWebSocketTransport(app)
+        ) as client:
+            async with aconnect_ws(client, "/ws") as ws:
+                await ws.send_json({"message": "CLIENT_MESSAGE"}, mode=mode)
+
+        on_receive_message.assert_called_once_with({"message": "CLIENT_MESSAGE"})
 
 
 @pytest.mark.asyncio
