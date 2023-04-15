28259d215a6d34da4a50106b6f72b387de2208df
François Voron <fvoron@gmail.com>
2023-04-15 13:46:46 +0200

    Remove useless docstring on event_loop fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -21,7 +21,6 @@ from starlette.routing import WebSocketRoute
 
 @pytest.fixture(scope="session")
 def event_loop():
-    """Force the pytest-asyncio loop to be the main one."""
     loop = asyncio.new_event_loop()
     yield loop
     loop.close()
